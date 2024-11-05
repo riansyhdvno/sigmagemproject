6508eef840333f970f383236334363e04426df89
Riansyah Divano <riansyahdivano29@gmail.com>
2024-11-05 09:36:39 +0700

    perbaiki tampilan reg dan login

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="h-full">
+<html lang="en" class="h-full bg-gray-800">
 
 <head>
     <meta charset="UTF-8">
@@ -12,15 +12,15 @@
     <title>Sign In</title>
 </head>
 
-<body class="h-full bg-gray-50 dark:bg-gray-900">
+<body class="h-full ">
     <section class="flex items-center justify-center px-6 py-8 md:h-screen lg:py-0">
-        <div class="w-full max-w-md bg-white rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
+        <div class="w-full max-w-md bg-gray-900 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
             <div class="p-6 md:p-8 space-y-6">
                 <div class="flex items-center justify-center mb-6">
                     <img class="w-8 h-8 mr-2" src="img/scz.png" alt="logo">
-                    <span class="text-2xl font-semibold text-gray-900 dark:text-white">SigmaGem</span>
+                    <span class="text-2xl font-semibold text-white">SigmaGem</span>
                 </div>
-                <h1 class="text-xl font-bold text-center text-gray-900 dark:text-white md:text-2xl">Sign In</h1>
+                <h1 class="text-xl font-bold text-center text-white md:text-2xl">Sign In</h1>
                 
                 {{-- Flash error message --}}
                 @if ($errors->has('error'))
@@ -32,12 +32,12 @@
                 <form method="POST" action="{{ route('login.post') }}" class="space-y-6">
                     @csrf
                     <div>
-                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                        <input type="email" name="email" id="email" class="w-full p-2.5 border border-gray-300 rounded-lg bg-gray-50 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-primary-600 focus:border-primary-600" placeholder="name@company.com" required>
+                        <label for="email" class="block mb-2 text-sm font-medium text-white">Email</label>
+                        <input type="email" name="email" id="email" class="w-full p-2.5 border-transparent border border-gray-300 rounded-lg  text-white bg-gray-800 dark:border-gray- dark:text-white focus:ring-primary-600 focus:border-primary-600" placeholder="name@company.com" required>
                     </div>
                     <div>
-                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password" name="password" id="password" class="w-full p-2.5 border border-gray-300 rounded-lg bg-gray-50 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-primary-600 focus:border-primary-600" placeholder="••••••••" required>
+                        <label for="password" class="block mb-2 text-sm font-medium text-white">Password</label>
+                        <input type="password" name="password" id="password" class="w-full p-2.5 border-transparent border border-gray-300 rounded-lg text-gray-900 bg-gray-800 dark:border-gray-600 dark:text-white focus:ring-primary-600 focus:border-primary-600" placeholder="••••••••" required>
                     </div>
                     <div class="flex items-center justify-between">
                         <div class="flex items-center">
--- resources/views/user/auth/register.blade.php
+++ resources/views/user/auth/register.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="en" class="h-full">
+<html lang="en" class="h-full bg-gray-800">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,60 +12,75 @@
     <title>Sign Up</title>
 </head>
 
-<body class="h-full flex items-center justify-center bg-gray-50 dark:bg-gray-900">
+<body class="h-full flex items-center justify-center ">
 
-    <section class="w-full max-w-md p-6 bg-white rounded-lg shadow dark:bg-gray-800">
-        <a href="#" class="flex items-center justify-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+    <section class="w-full max-w-md p-6 bg-gray-900 rounded-lg shadow dark:bg-gray-800">
+        <a href="#" class="flex items-center justify-center mb-6 text-2xl font-semibold text-white">
             <img class="w-8 h-8 mr-2" src="img/scz.png" alt="logo">
             SigmaGem
         </a>
 
-        <h1 class="text-xl font-bold text-center text-gray-900 dark:text-white mb-4">
+        <h1 class="text-xl font-bold text-center text-white mb-4">
             Registrasi Akun
         </h1>
-        <p class="text-center text-sm text-black dark:text-gray-400">Selamat datang di SigmaGem Consign</p>
+        <p class="text-center text-sm text-white">Selamat datang di SigmaGem Consign</p>
 
         <form class="space-y-4 mt-6" action="{{ route('register.post') }}" method="POST">
             @csrf
 
             @error('email')
-                 <div class="text-red-600">{{ $message }}</div>
+                <div class="text-red-600">{{ $message }}</div>
             @enderror
 
-            @if(session('error'))
-                <div class="p-4 mb-4 text-sm text-red-800 bg-red-50 rounded-lg dark:bg-gray-800 dark:text-red-400" role="alert">
+            @if (session('error'))
+                <div class="p-4 mb-4 text-sm text-red-800 bg-red-50 rounded-lg dark:bg-gray-800 dark:text-red-400"
+                    role="alert">
                     {{ session('error') }}
                 </div>
             @endif
 
             <div>
-                <label for="email" class="block text-sm mb-2 font-medium text-gray-900 dark:text-white">Email</label>
-                <input type="email" name="email" id="email" class="w-full p-2.5 border rounded-lg text-gray-900 dark:bg-gray-700" placeholder="name@email.com" required>
+                <label for="email" class="block text-sm mb-2 font-medium text-white">Email</label>
+                <input type="email" name="email" id="email"
+                    class="w-full p-2.5 border-transparent rounded-lg text-white bg-gray-800 placeholder-gray-500"
+                    placeholder="name@email.com" required>
             </div>
 
             <div>
-                <label for="fullname" class="block text-sm mb-2 font-medium text-gray-900 dark:text-white">Nama Lengkap</label>
-                <input type="text" name="full_name" id="fullname" class="w-full p-2.5 border rounded-lg text-gray-900 dark:bg-gray-700" placeholder="John Cena" required>
+                <label for="fullname" class="block text-sm mb-2 font-medium text-white">Nama
+                    Lengkap</label>
+                <input type="text" name="full_name" id="fullname"
+                    class="w-full p-2.5 border-transparent rounded-lg text-white bg-gray-800 placeholder-gray-500" placeholder="John Cena"
+                    required>
             </div>
 
             <div>
-                <label for="nohp" class="block text-sm mb-2 font-medium text-gray-900 dark:text-white">No Handphone</label>
-                <input type="number" name="no_hp" id="nohp" class="w-full p-2.5 border rounded-lg text-gray-900 dark:bg-gray-700" placeholder="08xxx" required>
+                <label for="nohp" class="block text-sm mb-2 font-medium text-white">No
+                    Handphone</label>
+                <input type="number" name="no_hp" id="nohp"
+                    class="w-full p-2.5 border-transparent rounded-lg text-white bg-gray-800 placeholder-gray-500" placeholder="08xxx" required>
             </div>
 
             <div>
-                <label for="password" class="block text-sm mb-2 font-medium text-gray-900 dark:text-white">Password</label>
-                <input type="password" name="password" id="password" class="w-full p-2.5 border rounded-lg text-gray-900 dark:bg-gray-700" placeholder="••••••••" required>
+                <label for="password"
+                    class="block text-sm mb-2 font-medium text-white">Password</label>
+                <input type="password" name="password" id="password"
+                    class="w-full p-2.5 border-transparent rounded-lg text-white bg-gray-800 placeholder-gray-500" placeholder="••••••••"
+                    required>
             </div>
 
-            <button type="submit" class="w-full px-5 py-2.5 text-sm font-medium text-white bg-purple-600 rounded-lg text-center">Registrasi Akun</button>
+            <button type="submit"
+                class="w-full px-5 py-2.5 border-transparent text-sm font-medium text-white bg-purple-600 rounded-lg text-center">Registrasi
+                Akun</button>
 
             <p class="text-sm font-light text-center text-gray-500 dark:text-gray-400">
-                Sudah punya akun? <a href="{{ route('login') }}" class="font-medium text-primary-600 dark:text-primary-500">Login</a>
+                Sudah punya akun? <a href="{{ route('login') }}"
+                    class="font-medium text-primary-600 dark:text-primary-500">Login</a>
             </p>
         </form>
     </section>
 
     <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
 </body>
+
 </html>
